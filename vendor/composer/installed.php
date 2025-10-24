698a3f4e5ac0e60f9927fe7d42b265759fc8b870
Italo-fullStack <italosoares1p@gmail.com>
2025-10-24 11:30:33 -0300

    final commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pichau/mvc',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '660179cb003f46f30685bb28e7952b6c1831d3cb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pichau/mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '660179cb003f46f30685bb28e7952b6c1831d3cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
